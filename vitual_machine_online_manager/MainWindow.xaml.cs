910dae3efd9762a492fda5994e2e7c60d9a9001c
luong hai dang <s1abmmo@gmail.com>
2021-10-12 00:49:32 +0700

    Add project files.

---
+++ vitual_machine_online_manager/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using vitual_machine_online_manager.Model;
+
+namespace vitual_machine_online_manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var cc = new VitualMachine(name: "DMM");
+            MessageBox.Show(cc.lastTimePing.ToString());
+        }
+    }
+}
---
+++ vitual_machine_online_manager/Model/VitualMachine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vitual_machine_online_manager.Model
+{
+    class VitualMachine
+    {
+        public String name { get; private set; }
+        public DateTime? lastTimePing { get; private set; }
+        public TimeSpan? durationFromLastTimePing { get; private set; }
+        public bool? isOk { get; private set; }
+        public List<String> listNameScreenshot { get; private set; }
+
+        public VitualMachine(String name)
+        {
+            this.name = name;
+            this.lastTimePing = null;
+            this.durationFromLastTimePing = null;
+            this.isOk = null;
+            this.listNameScreenshot = listNameScreenshot ?? new List<String>();
+        }
+
+        public void Load(DateTime? lastTimePing, TimeSpan? durationFromLastTimePing, bool? isOk, List<String>? listNameScreenshot)
+        {
+            this.lastTimePing = lastTimePing;
+            this.durationFromLastTimePing = durationFromLastTimePing;
+            this.isOk = isOk;
+            this.listNameScreenshot = listNameScreenshot ?? new List<String>();
+        }
+
+    }
+}
